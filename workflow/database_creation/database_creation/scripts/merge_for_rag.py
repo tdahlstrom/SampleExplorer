c45c7fc04fe3289b5804d911749ef95840ba328b
Thomas Dahlstrom <tdahlstrom@gmail.com>
2025-10-27 13:26:40 -0700

    Update file paths to use snakemake inputs/outputs

--- workflow/database_creation/database_creation/scripts/merge_for_rag.py
+++ workflow/database_creation/database_creation/scripts/merge_for_rag.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import snakemake
 
-comb = pd.read_csv("results/full_nosuperseries_with_pmid.csv", index_col=0)
+comb = pd.read_csv(snakemake.input[0], index_col=0)
 
 comb.columns = ['Unnamed: 0', 'series_id', 'characteristics_ch1',
        'extract_protocol_ch1', 'source_name_ch1', 'title_ex', 'title', 'summary',
@@ -9,7 +10,7 @@ comb.columns = ['Unnamed: 0', 'series_id', 'characteristics_ch1',
 super1 = comb[['series_id', 'characteristics_ch1','title', 'summary',
        'overall_design', 'pmid']].drop_duplicates()
 
-comb2 = pd.read_csv("results/full_superseries_with_pmid.csv", index_col=0)
+comb2 = pd.read_csv(snakemake.input[1], index_col=0)
 
 comb2.columns = ['Unnamed: 0', 'series_id', 'characteristics_ch1',
        'extract_protocol_ch1', 'source_name_ch1', 'title_ex', 'title', 'summary',
@@ -20,4 +21,4 @@ super2 = comb2[['series_id', 'characteristics_ch1','title', 'summary',
 
 for_RAG = pd.concat([super1, super2])
 
-for_RAG.to_csv("results/RAG.csv")
+for_RAG.to_csv(snakemake.output[0])
