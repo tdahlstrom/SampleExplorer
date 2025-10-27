a9002d34cee7a860199ba433c23d277bca23cdd2
Thomas Dahlstrom <tdahlstrom@gmail.com>
2025-10-27 13:30:32 -0700

    Integrate snakemake for input and output file handling

--- workflow/database_creation/database_creation/scripts/create_rag_binaries.py
+++ workflow/database_creation/database_creation/scripts/create_rag_binaries.py
@@ -2,8 +2,9 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 import pickle
+import snakemake
 
-RAG_data = pd.read_csv("results/RAG.csv")
+RAG_data = pd.read_csv(snakemake.input[0])
 
 smaller_df_for_RAG = RAG_data[["title", "summary", "overall_design","series_id"]]
 
@@ -11,7 +12,7 @@ smaller_df_for_RAG["rag_text"] = RAG_data["title"] + "\n." + RAG_data["summary"]
 
 rag_input = smaller_df_for_RAG[['series_id', 'rag_text']].drop_duplicates()
 
-rag_input.to_pickle("results/rag_index.p")
+rag_input.to_pickle(snakemake.output[0])
 
 from sentence_transformers import SentenceTransformer
 model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
@@ -25,7 +26,7 @@ model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
 sentences = rag_input["rag_text"].to_list()
 embedding = model.encode(sentences)
 
-file_path = "results/rag_embedding_matrix.pkl"
+file_path = "snakemake.output[1]"
 
 # Save the matrix to a pickled file
 with open(file_path, 'wb') as file:
